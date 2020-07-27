4d9be9bf1c180d5c0d58b7119186f70c5199bfe1
Hassan <hasrayrah970@gmail.com>
2020-07-27 19:59:06 +0500

    second commit

--- pizza/settings.py
+++ pizza/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pizza.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static ')
+    os.path.join(BASE_DIR, "static")
 ]
