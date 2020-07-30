5aa8ee67ee39c1bff46ae51df70a74730c671df2
Hassan <hasrayrah970@gmail.com>
2020-07-30 19:47:34 +0500

    new

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import CreateMenuItem
 # Create your views here.
 
 def home(request):
     return render(request, 'main/index.html')
 
 def menu(request):
-    return render(request, 'main/menu.html')
+    items = CreateMenuItem.objects.all()
+    return render(request, 'main/menu.html', {'items':items})
--- pizza/settings.py
+++ pizza/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Pizza',
+        'USER': 'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost'
     }
 }
 
@@ -123,3 +126,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
