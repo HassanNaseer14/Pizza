3d4a8106c5cc89f0ffaee8dd746c2143313000f7
Hassan <hasrayrah970@gmail.com>
2020-08-02 20:59:20 +0500

    ie

--- pizza/settings.py
+++ pizza/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2f_f(b_h5r_yt2^%9iv53irwsg#bu)#53bcy2u9rpole-^0djv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
